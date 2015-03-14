ef9fd5f1d545170927d9ddbdb6c6a96ef0c515cd
hernantorrisi@gmail.com <htorrisi@panareadigital.com>
2015-03-14 14:06:57 -0300

    point on line check should be absolute

--- player/js/utils/DataManager.js
+++ player/js/utils/DataManager.js
@@ -279,7 +279,7 @@ var dataManager = (function(){
     }
 
     function pointOnLine2D(x1,y1, x2,y2, x3,y3){
-        return ((x2 - x1) * (y3 - y1)) - ((x3 - x1) * (y2 - y1)) < 0.0000001;
+        return Math.abs(((x2 - x1) * (y3 - y1)) - ((x3 - x1) * (y2 - y1))) < 0.0000001;
     }
 
     function buildBezierData(keyData){
