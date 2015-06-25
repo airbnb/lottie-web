7802593551853555d03a7ecc5e68623bb665c929
hernantorrisi@gmail.com <htorrisi@panareadigital.com>
2015-06-25 13:54:20 -0300

    new player build

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ var concat = require('gulp-concat');
 var watch = require('gulp-watch');
 
 gulp.task('gzipFile', function(){
-  //gulp.src('demo/dalek/data.js')
+  //gulp.src('demo/murally/data.js')
   gulp.src('build/player/bodymovin.js')
     .pipe(gzip({ append: false }))
     .pipe(gulp.dest('demo/'));
