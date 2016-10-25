3dbbca492bdd6541ed2f8c4c292ccd886c446223
hernan <hernantorrisi@gmail.com>
2016-10-25 08:42:00 -0300

    standalone fix

--- extension/jsx/dataManager.jsx
+++ extension/jsx/dataManager.jsx
@@ -217,8 +217,8 @@ var bm_dataManager = (function () {
         }
         if (config.standalone) {
             var bodymovinJsStr = bm_downloadManager.getStandaloneData();
-            string = bodymovinJsStr.replace("'__[ANIMATIONDATA]__'",  string );
-            string = string.replace("'__[STANDALONE]__'", 'true');
+            string = bodymovinJsStr.replace("\"__[ANIMATIONDATA]__\"",  string );
+            string = string.replace("\"__[STANDALONE]__\"", 'true');
         }
         ////
         try {
