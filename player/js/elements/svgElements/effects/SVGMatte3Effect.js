420e31750a432c37f318b9cc357c769b22cecb41
hernan <hernantorrisi@gmail.com>
2017-12-25 21:05:18 -0500

    svg matte effect improved support

--- player/js/elements/svgElements/effects/SVGMatte3Effect.js
+++ player/js/elements/svgElements/effects/SVGMatte3Effect.js
@@ -1,3 +1,6 @@
+var _svgMatteSymbols = [];
+var _svgMatteMaskCounter = 0;
+
 function SVGMatte3Effect(filterElem, filterManager, elem){
     this.initialized = false;
     this.filterManager = filterManager;
@@ -9,22 +12,71 @@ function SVGMatte3Effect(filterElem, filterManager, elem){
     elem.baseElement = elem.matteElement;
 }
 
+SVGMatte3Effect.prototype.findSymbol = function(mask) {
+    var i = 0, len = _svgMatteSymbols.length;
+    while(i < len) {
+        if(_svgMatteSymbols[i] === mask) {
+            return _svgMatteSymbols[i];
+        }
+        i += 1;
+    }
+    return null;
+}
+
+SVGMatte3Effect.prototype.replaceInParent = function(mask, symbolId) {
+    var parentNode = mask.layerElement.parentNode;
+    if(!parentNode) {
+        return;
+    }
+    var children = parentNode.children;
+    var i = 0, len = children.length;
+    while (i < len) {
+        if (children[i] === mask.layerElement) {
+            break;
+        }
+        i += 1;
+    }
+    var nextChild;
+    if (i <= len - 2) {
+        nextChild = children[i + 1];
+    }
+    var useElem = createNS('use');
+    useElem.setAttribute('href', '#' + symbolId);
+    if(nextChild) {
+        parentNode.insertBefore(useElem, nextChild);
+    } else {
+        parentNode.appendChild(useElem);
+    }
+}
+
 SVGMatte3Effect.prototype.setElementAsMask = function(elem, mask) {
-    var masker = createNS('mask');
-    masker.setAttribute('id',mask.layerId);
-    masker.setAttribute('mask-type','alpha');
-    masker.appendChild(mask.layerElement);
+    if(!this.findSymbol(mask)) {
+        var symbolId = 'matte_' + randomString(5) + '_' + _svgMatteMaskCounter++;
+        var masker = createNS('mask');
+        masker.setAttribute('id', mask.layerId);
+        masker.setAttribute('mask-type', 'alpha');
+        _svgMatteSymbols.push(mask);
+        var defs = elem.globalData.defs;
+        defs.appendChild(masker);
+        var symbol = createNS('symbol');
+        symbol.setAttribute('id', symbolId);
+        this.replaceInParent(mask, symbolId);
+        symbol.appendChild(mask.layerElement);
+        defs.appendChild(symbol);
+        useElem = createNS('use');
+        useElem.setAttribute('href', '#' + symbolId);
+        masker.appendChild(useElem);
+        mask.data.hd = false;
+        mask.show();
+    }
     elem.setMatte(mask.layerId);
-    mask.data.hd = false;
-    var defs = elem.globalData.defs;
-    defs.appendChild(masker);
 }
 
 SVGMatte3Effect.prototype.initialize = function() {
     var ind = this.filterManager.effectElements[0].p.v;
     var i = 0, len = this.elem.comp.elements.length;
-    while(i < len) {
-    	if(this.elem.comp.elements[i].data.ind === ind) {
+    while (i < len) {
+    	if (this.elem.comp.elements[i].data.ind === ind) {
     		this.setElementAsMask(this.elem, this.elem.comp.elements[i]);
     	}
     	i += 1;
