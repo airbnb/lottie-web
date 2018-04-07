0ebf848536b6bc608576ec755cf7b2305d6f744f
hernan <hernantorrisi@gmail.com>
2018-04-07 13:30:19 -0400

    moved renderers to elements renderer

---
+++ player/js/utils/shapes/shapePathBuilder.js
@@ -0,0 +1,17 @@
+var buildShapeString = function(pathNodes, length, closed, mat) {
+	if(length === 0) {
+            return '';
+        }
+        var _o = pathNodes.o;
+        var _i = pathNodes.i;
+        var _v = pathNodes.v;
+        var i, shapeString = " M" + mat.applyToPointStringified(_v[0][0], _v[0][1]);
+        for(i = 1; i < length; i += 1) {
+            shapeString += " C" + mat.applyToPointStringified(_o[i - 1][0], _o[i - 1][1]) + " " + mat.applyToPointStringified(_i[i][0], _i[i][1]) + " " + mat.applyToPointStringified(_v[i][0], _v[i][1]);
+        }
+        if (closed && length) {
+            shapeString += " C" + mat.applyToPointStringified(_o[i - 1][0], _o[i - 1][1]) + " " + mat.applyToPointStringified(_i[0][0], _i[0][1]) + " " + mat.applyToPointStringified(_v[0][0], _v[0][1]);
+            shapeString += 'z';
+        }
+        return shapeString;
+}
