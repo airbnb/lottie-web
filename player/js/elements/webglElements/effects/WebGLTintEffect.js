5f63259cbedb9532b75e44a5bfa6cee68c774406
hernan <hernantorrisi@gmail.com>
2018-07-10 22:20:26 -0400

    tint effect

--- player/js/elements/webglElements/effects/WebGLTintEffect.js
+++ player/js/elements/webglElements/effects/WebGLTintEffect.js
@@ -15,63 +15,70 @@ function WTintFilter(filterManager, gl){
     fsh += 'uniform sampler2D u_image;';
     fsh += 'varying vec2 v_texCoord;';
     fsh += 'uniform vec2 u_textureSize;';
-    fsh += 'uniform float u_kernel[9];';
-    fsh += 'uniform float u_kernelWeight;';
+    fsh += 'uniform float color_amount;';
+    fsh += 'uniform vec4 whiteToColor;';
+    fsh += 'uniform vec4 blackToColor;';
 
     fsh += 'void main() {';
-    fsh += 'vec2 resolution = vec2(278.0, 278.0);';
-    fsh += 'vec2 onePixel = vec2(1.0, 1.0) / resolution;';
-    fsh += 'vec4 colorSum = texture2D(u_image, v_texCoord + onePixel * vec2(-1, -1)) * u_kernel[0] +'
-    fsh += 'texture2D(u_image, v_texCoord + onePixel * vec2( 0, -1)) * u_kernel[1] +'
-    fsh += 'texture2D(u_image, v_texCoord + onePixel * vec2( 1, -1)) * u_kernel[2] +'
-    fsh += 'texture2D(u_image, v_texCoord + onePixel * vec2(-1,  0)) * u_kernel[3] +'
-    fsh += 'texture2D(u_image, v_texCoord + onePixel * vec2( 0,  0)) * u_kernel[4] +'
-    fsh += 'texture2D(u_image, v_texCoord + onePixel * vec2( 1,  0)) * u_kernel[5] +'
-    fsh += 'texture2D(u_image, v_texCoord + onePixel * vec2(-1,  1)) * u_kernel[6] +'
-    fsh += 'texture2D(u_image, v_texCoord + onePixel * vec2( 0,  1)) * u_kernel[7] +'
-    fsh += 'texture2D(u_image, v_texCoord + onePixel * vec2( 1,  1)) * u_kernel[8] ;'
-    fsh += 'gl_FragColor = vec4((colorSum / u_kernelWeight).rgb, 1);'
+    fsh += 'vec4 textureValue = texture2D(u_image, v_texCoord);'
+    fsh += 'float saturation = (textureValue.r + textureValue.g + textureValue.b) / 3.0;'
+    fsh += 'float r_component = blackToColor.r + (whiteToColor.r - blackToColor.r) * saturation;'
+    fsh += 'float g_component = blackToColor.g + (whiteToColor.g - blackToColor.g) * saturation;'
+    fsh += 'float b_component = blackToColor.b + (whiteToColor.b - blackToColor.b) * saturation;'
+    fsh += 'gl_FragColor = vec4(r_component * color_amount + textureValue.r * (1.0 - color_amount),'
+    fsh += 'g_component * color_amount + textureValue.g * (1.0 - color_amount),'
+    fsh += 'b_component * color_amount + textureValue.b * (1.0 - color_amount),'
+    fsh += '1);'
     fsh += '}';
 
     var vertexShader = WebGLProgramFactory.createShader(gl, gl.VERTEX_SHADER, vsh);
     var fragmentShader = WebGLProgramFactory.createShader(gl, gl.FRAGMENT_SHADER, fsh);
     this.program = WebGLProgramFactory.createProgram(gl, vertexShader, fragmentShader);
 
-    var kernel = [-1, -1, -1,
-        2,  2,  2,
-       -1, -1, -1];
     gl.useProgram(this.program);
     this.positionAttributeLocation = gl.getAttribLocation(this.program, "a_position");
     gl.enableVertexAttribArray(this.positionAttributeLocation);
-    var kernelLocation = gl.getUniformLocation(this.program, "u_kernel");
-    gl.uniform1fv(kernelLocation, kernel);
-    var kernelWeightLocation = gl.getUniformLocation(this.program, "u_kernelWeight");
-    gl.uniform1f(kernelWeightLocation, computeKernelWeight(kernel));
-    var textureSizeLocation = gl.getUniformLocation(this.program, "u_textureSize");
-    gl.uniform2f(textureSizeLocation, 278, 278);
-    ////
-
-    ////
     gl.vertexAttribPointer(this.positionAttributeLocation, 2,gl.FLOAT, false, 0, 0);
+
+
+    this.blackToColorLoc = gl.getUniformLocation(this.program, "blackToColor");
+    this.whiteToColorLoc = gl.getUniformLocation(this.program, "whiteToColor");
+    this.amountLoc = gl.getUniformLocation(this.program, "color_amount");
+    gl.uniform1f(this.amountLoc, 0.5);
+
 
     this.filterManager = filterManager;
     this.gl = gl;
-    
+    this.currentBlackColor = [-1,-1,-1,-1];
+    this.currentWhiteColor = [-1,-1,-1,-1];
+    this.currentAmount = -1;
 }
-
-function computeKernelWeight(kernel) {
-    var weight = kernel.reduce(function(prev, curr) {
-        return prev + curr;
-    });
-    return weight <= 0 ? 1 : weight;
-  }
 
 WTintFilter.prototype.renderFrame = function(forceRender, buffer){
     this.gl.useProgram(this.program);
+    var col = this.filterManager.effectElements[0].p.v;
+    var currCol = this.currentBlackColor;
+    if(currCol[0] !== col[0] || currCol[1] !== col[1] || currCol[2] !== col[2]) {
+        this.gl.uniform4fv(this.blackToColorLoc, col);
+        currCol[0] = col[0];
+        currCol[1] = col[1];
+        currCol[2] = col[2];
+    }
+    col = this.filterManager.effectElements[1].p.v;
+    currCol = this.currentWhiteColor;
+    if(currCol[0] !== col[0] || currCol[1] !== col[1] || currCol[2] !== col[2]) {
+        this.gl.uniform4fv(this.whiteToColorLoc, col);
+        currCol[0] = col[0];
+        currCol[1] = col[1];
+        currCol[2] = col[2];
+    }
+    if(this.currentAmount !== this.filterManager.effectElements[2].p.v/100) {
+        this.gl.uniform1f(this.amountLoc, this.filterManager.effectElements[2].p.v/100);
+        this.currentAmount = this.filterManager.effectElements[2].p.v/100;
+    }
     this.gl.drawArrays(this.gl.TRIANGLES, 0, 6);
          
     //drawWithKernel();
     if(forceRender || this.filterManager._mdf){
-
     }
 };
