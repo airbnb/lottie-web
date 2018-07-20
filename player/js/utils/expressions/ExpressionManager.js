a06a5b6a90d75feae84b62c6dcec534ba0d287a4
hernan <hernantorrisi@gmail.com>
2018-07-19 20:16:55 -0400

    removed log

--- player/js/utils/expressions/ExpressionManager.js
+++ player/js/utils/expressions/ExpressionManager.js
@@ -571,7 +571,6 @@ var ExpressionManager = (function(){
             if (!thisLayer) {
                 text = elem.layerInterface.text;
                 thisLayer = elem.layerInterface;
-                console.log(thisLayer)
                 thisComp = elem.comp.compInterface;
                 toWorld = thisLayer.toWorld.bind(thisLayer);
                 fromWorld = thisLayer.fromWorld.bind(thisLayer);
