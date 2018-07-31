747bda448f6b2679e1ee4c214cbe6d1c566c1c50
hernan <hernantorrisi@gmail.com>
2018-07-30 22:35:56 -0400

    update

--- player/js/elements/webglElements/effects/WebGLPuppetPin.js
+++ player/js/elements/webglElements/effects/WebGLPuppetPin.js
@@ -22,49 +22,45 @@ function WPuppetPinEffect(filterManager, elem) {
     gl.enableVertexAttribArray(this.positionAttributeLocation);
     this.texcoordLocation = gl.getAttribLocation(this.program, "a_texCoord");
     gl.enableVertexAttribArray(this.texcoordLocation);
+    this.positionBuffer = gl.createBuffer();
 
     this.filterManager = filterManager;
     this.gl = gl;
     this.elem = elem;
 
-    var positions = [];
-    var i, j, len = 10, jlen = 10;
-    for(j = 0; j < jlen; j += 1) {
-        for(i = 0; i < len; i += 1) {
-            /*positions.push(i/10);
-            positions.push(j/10);
-            positions.push((i+1)/10);
-            positions.push(j/10);
-            positions.push(i/10);
-            positions.push((j+1)/10);
-            positions.push((i+1)/10);
-            positions.push(j/10);
-            positions.push((i+1)/10);
-            positions.push((j+1)/10);
-            positions.push(i/10);
-            positions.push((j+1)/10);*/
-            positions.push(Math.random());
-            positions.push(Math.random());
-            positions.push(Math.random());
-            positions.push(Math.random());
-            positions.push(Math.random());
-            positions.push(Math.random());
-            positions.push(Math.random());
-            positions.push(Math.random());
-            positions.push(Math.random());
-            positions.push(Math.random());
-            positions.push(Math.random());
-            positions.push(Math.random());
+    this.MASSES_PER_ROW = 20;
+
+    this.particles = [];
+    var i, j, len = this.MASSES_PER_ROW;
+    var restingLength = 1 / (this.MASSES_PER_ROW - 1);
+    restingLength /= 20;
+    for(j = 0; j < len ; j += 1) {
+        for(i = 0; i < len ; i += 1) {
+            var p = new WPuppetPinParticle(i / (len - 1), j / (len - 1), this.particles);
+            this.particles.push(p);
+            // Connect left and right
+            if ( i > 0 ) { p.addSpring(this.particles.length - 2, restingLength) }
+            if ( i < len - 1 ) { p.addSpring(this.particles.length, restingLength) }
+            // Connect up and down
+            if ( j > 0 ) { p.addSpring(this.particles.length - 1 - len, restingLength) }
+            if ( j < len - 1 ) { p.addSpring(this.particles.length - 1 + len, restingLength) }
+            // Diagonal springs
+            var diagonal = Math.SQRT2 * restingLength; 
+            if ( i > 0 && j > 0) { p.addSpring(this.particles.length - 2 - len, diagonal) }
+            if ( i < len - 1 && j > 0) { p.addSpring(this.particles.length - len, diagonal) }
+            if ( i > 0 && j < len - 1) { p.addSpring(this.particles.length - 2 + len, diagonal) }
+            if ( i < len - 1 && j < len - 1) { p.addSpring(this.particles.length + len, diagonal) }
         }   
     }
-    this.positions = new Float32Array(positions);
-    this.positionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, this.positionBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, this.positions, gl.STATIC_DRAW);
+    this.positions = createTypedArray('float32', this.particles.length * 12);
+    this.particles[0].fixed = true;
+    this.particles[this.MASSES_PER_ROW - 1].fixed = true;
+    this.particles[this.MASSES_PER_ROW * this.MASSES_PER_ROW - 1].fixed = true;
+    this.particles[this.MASSES_PER_ROW * this.MASSES_PER_ROW - this.MASSES_PER_ROW].fixed = true;
 
     var texture_positions = [];
-    var i, j, len = 10, jlen = 10;
-    for(j = 0; j < jlen; j += 1) {
+    var i, j, len = (this.MASSES_PER_ROW - 1);
+    for(j = 0; j < len; j += 1) {
         for(i = 0; i < len; i += 1) {
             /*texture_positions.push(Math.random());
             texture_positions.push(Math.random());
@@ -78,18 +74,18 @@ function WPuppetPinEffect(filterManager, elem) {
             texture_positions.push(Math.random());
             texture_positions.push(Math.random());
             texture_positions.push(Math.random());*/
-            texture_positions.push(i/10);
-            texture_positions.push(j/10);
-            texture_positions.push((i+1)/10);
-            texture_positions.push(j/10);
-            texture_positions.push(i/10);
-            texture_positions.push((j+1)/10);
-            texture_positions.push((i+1)/10);
-            texture_positions.push(j/10);
-            texture_positions.push((i+1)/10);
-            texture_positions.push((j+1)/10);
-            texture_positions.push(i/10);
-            texture_positions.push((j+1)/10);
+            texture_positions.push(i/len);
+            texture_positions.push(j/len);
+            texture_positions.push((i+1)/len);
+            texture_positions.push(j/len);
+            texture_positions.push(i/len);
+            texture_positions.push((j+1)/len);
+            texture_positions.push((i+1)/len);
+            texture_positions.push(j/len);
+            texture_positions.push((i+1)/len);
+            texture_positions.push((j+1)/len);
+            texture_positions.push(i/len);
+            texture_positions.push((j+1)/len);
         }   
     }
     this.texture_positions = new Float32Array(texture_positions);
@@ -98,14 +94,68 @@ function WPuppetPinEffect(filterManager, elem) {
     gl.bufferData(gl.ARRAY_BUFFER, this.texture_positions, gl.STATIC_DRAW);
 }
 
-function WPuppetPinParticle(x, y) {
+function WPuppetPinParticle(x, y, particles) {
     this.x = x;
     this.y = y;
+    this.force = [0, 0];
+    this.springs = [];
+    this.particles = particles;
+    this.fixed = false;
 }
 
-WPuppetPinParticle.prototype.addForce = function(x, y) {
-    this.x += x;
-    this.y += y;
+WPuppetPinParticle.prototype.addSpring = function(target, restingLength) {
+  this.springs.push({
+    target: target,
+    restingLength: restingLength
+  });
+}
+
+WPuppetPinParticle.prototype.addForce = function(force) {
+    this.x += force[0];
+    this.y += force[1];
+}
+
+WPuppetPinParticle.prototype.distanceTo = function(other) {
+  var deltaX = this.x - other.x;
+  var deltaY = this.y - other.y;
+  return Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+}
+
+WPuppetPinParticle.prototype.resolveForce = function() {
+  var F = this.force;
+  F[0] = 0;
+  F[1] = 0;
+  var k = 0.1; // Spring constant
+  for(var s = 0; s < this.springs.length; s += 1) {
+    var spring = this.springs[s];
+    var other = this.particles[spring.target];
+    var D = this.distanceTo(other);
+    var extension = D - spring.restingLength;
+    var force = -k * extension;
+    F[0] += force * (this.x - other.x) / D;
+    F[1] += force * (this.y - other.y) / D;
+  }
+
+  return F;
+}
+
+WPuppetPinEffect.prototype.simulationStep = function(){
+    // Sweeping across particles, calculate forces
+    var i, len = this.particles.length, particles;
+    for(i = 0; i < len; i += 1) {
+        particle = this.particles[i];
+        if (!particle.fixed) {
+          particle.resolveForce();
+        }
+    }
+      
+    // Apply forces and update positions
+    for(i = 0; i < len; i += 1) {
+        particle = this.particles[i];
+        if (!particle.fixed) {
+          particle.addForce(particle.force);
+        }
+    }
 }
 
 WPuppetPinEffect.prototype.renderFrame = function(forceRender, buffer){
@@ -114,36 +164,39 @@ WPuppetPinEffect.prototype.renderFrame = function(forceRender, buffer){
     this.gl.useProgram(this.program);
 
     //POSITION
-    var particles = []
     var positions = [];
-    var i, j, len = 10, jlen = 10;
-    for(j = 0; j < jlen; j += 1) {
-        for(i = 0; i < len; i += 1) {
-            particles.push(new WPuppetPinParticle(i / 10, j / 10));
-            particles.push(new WPuppetPinParticle((i + 1) / 10, j / 10));
-            particles.push(new WPuppetPinParticle(i / 10, (j + 1) / 10));
-            particles.push(new WPuppetPinParticle((i + 1) / 10, j / 10));
-            particles.push(new WPuppetPinParticle((i + 1) / 10, (j + 1) / 10));
-            particles.push(new WPuppetPinParticle(i / 10, (j + 1) / 10));
+    //var size = 1 / this.MASSES_PER_ROW;
+    var i = 0;
 
-            /*particles.push(new WPuppetPinParticle(Math.random(), Math.random()));
-            particles.push(new WPuppetPinParticle(Math.random(), Math.random()));
-            particles.push(new WPuppetPinParticle(Math.random(), Math.random()));
-            particles.push(new WPuppetPinParticle(Math.random(), Math.random()));
-            particles.push(new WPuppetPinParticle(Math.random(), Math.random()));
-            particles.push(new WPuppetPinParticle(Math.random(), Math.random()));*/
+    for(i = 0; i < 10; i += 1) {
+      this.simulationStep();
+    }
+
+    var len = this.MASSES_PER_ROW;
+    var _count = 0;
+    var particle,next_x_particle,next_y_particle,next_x_y_particle;
+    for(var j = 0; j < len - 1; j += 1) {
+        for(i = 0; i < len - 1; i += 1) {
+            particle = this.particles[j * len + i];
+            next_x_particle = this.particles[j * len + i + 1];
+            next_y_particle = this.particles[(j + 1) * len + i];
+            next_x_y_particle = this.particles[(j + 1) * len + i + 1];
             
+            this.positions[_count++] = particle.x;
+            this.positions[_count++] = particle.y;
+            this.positions[_count++] = next_x_particle.x;
+            this.positions[_count++] = next_x_particle.y;
+            this.positions[_count++] = next_y_particle.x;
+            this.positions[_count++] = next_y_particle.y;
+            this.positions[_count++] = next_x_particle.x;
+            this.positions[_count++] = next_x_particle.y;
+            this.positions[_count++] = next_x_y_particle.x;
+            this.positions[_count++] = next_x_y_particle.y;
+            this.positions[_count++] = next_y_particle.x;
+            this.positions[_count++] = next_y_particle.y;
         }   
     }
-    for(i = 0; i < particles.length; i += 1) {
-        if(i > 40 && i < 70) {
-            particles[i].addForce(0.01,0.01);
-        }
-        positions.push(particles[i].x);
-        positions.push(particles[i].y);
-    }
-    this.positions = new Float32Array(positions);
-    this.positionBuffer = gl.createBuffer();
+
     gl.bindBuffer(gl.ARRAY_BUFFER, this.positionBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, this.positions, gl.STATIC_DRAW);
     gl.enableVertexAttribArray(this.positionAttributeLocation);
@@ -153,6 +206,6 @@ WPuppetPinEffect.prototype.renderFrame = function(forceRender, buffer){
     gl.enableVertexAttribArray(this.texcoordLocation);
     gl.vertexAttribPointer(this.texcoordLocation, 2, gl.FLOAT, false, 0, 0);
     //DRAWING
-    this.gl.drawArrays(this.gl.TRIANGLES, 0, 6 * 100);
+    this.gl.drawArrays(this.gl.TRIANGLES, 0, 6 * (this.MASSES_PER_ROW - 1) * (this.MASSES_PER_ROW - 1));
     gl.bindBuffer(gl.ARRAY_BUFFER, this.elem.globalData.positionBuffer);
 };
