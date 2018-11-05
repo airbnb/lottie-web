915944c26a89a92883cf69e66b667a3359928bcf
Alexandre Monjol <alexandre.monjol@qonto.eu>
2018-11-05 15:15:50 +0100

    Fix formatResponse if responseText

--- build/player/lottie_light.js
+++ build/player/lottie_light.js
@@ -4306,47 +4306,46 @@ var filtersFactory = (function(){
 
 	return ob;
 }());
-var assetLoader = (function(){
+var assetLoader = (function() {
+  function formatResponse(xhr) {
+    if (xhr.response && typeof xhr.response === "object") {
+      return xhr.response;
+    } else if (xhr.response && typeof xhr.response === "string") {
+      return JSON.parse(xhr.response);
+    } else if (xhr.responseText) {
+      return JSON.parse(xhr.responseText);
+    }
+  }
 
-	function formatResponse(xhr) {
-		if(xhr.response && typeof xhr.response === 'object') {
-			return xhr.response;
-		} else if(xhr.response && typeof xhr.response === 'string') {
-			return JSON.parse(xhr.response);
-		} else if(xhr.responseText) {
-			return JSON.parse(xhr.response);
-		}
-	}
-
-	function loadAsset(path, callback, errorCallback) {
-		var response;
-		var xhr = new XMLHttpRequest();
-		xhr.open('GET', path, true);
-		// set responseType after calling open or IE will break.
-		xhr.responseType = "json";
-	    xhr.send();
-	    xhr.onreadystatechange = function () {
-	        if (xhr.readyState == 4) {
-	            if(xhr.status == 200){
-	            	response = formatResponse(xhr);
-	            	callback(response);
-	            }else{
-	                try{
-	            		response = formatResponse(xhr);
-	            		callback(response);
-	                }catch(err){
-	                	if(errorCallback) {
-	                		errorCallback(err);
-	                	}
-	                }
-	            }
-	        }
-	    };
-	}
-	return {
-		load: loadAsset
-	}
-}())
+  function loadAsset(path, callback, errorCallback) {
+    var response;
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", path, true);
+    // set responseType after calling open or IE will break.
+    xhr.responseType = "json";
+    xhr.send();
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4) {
+        if (xhr.status == 200) {
+          response = formatResponse(xhr);
+          callback(response);
+        } else {
+          try {
+            response = formatResponse(xhr);
+            callback(response);
+          } catch (err) {
+            if (errorCallback) {
+              errorCallback(err);
+            }
+          }
+        }
+      }
+    };
+  }
+  return {
+    load: loadAsset
+  };
+})();
 
 function TextAnimatorProperty(textData, renderType, elem){
     this._isFirstFrame = true;
@@ -9572,4 +9571,4 @@ function EffectsManager(){}
     }
     var readyStateCheckInterval = setInterval(checkReady, 100);
     return lottiejs;
-}));
+}));
