ebc3acbf3cdab885d00cb5ebebc7056bb6c912f4
SayBGM <bgm537@saybgm.me>
2020-04-23 00:33:32 +0900

    Updates on AnimationItem definition

--- index.d.ts
+++ index.d.ts
@@ -4,21 +4,91 @@ export type AnimationEventName = 'enterFrame' | 'loopComplete' | 'complete' | '
 export type AnimationEventCallback<T = any> = (args: T) => void;
 
 export type AnimationItem = {
-    play(): void;
-    stop(): void;
-    pause(): void;
+    name: string;
+    path: string;
+    isLoaded: boolean;
+    currentFrame: number;
+    currentRawFrame: number;
+    firstFrame: number;
+    totalFrames: number;
+    frameRate: number;
+    frameMult: number;
+    playSpeed: number;
+    playDirection: 1;
+    playCount: number;
+    isPaused: boolean;
+    autoplay: boolean;
+    loop: boolean;
+    renderer: any;
+    animationID: string;
+    assetsPath: string;
+    timeCompleted: 0;
+    segmentPos: 0;
+    subframeEnabled, boolean;
+    segments: AnimationSegment | AnimationSegment[];
+    _idle: boolean;
+    _completedLoop: boolean;
+    animationData: any;
+    assets: any;
+    imagePreloader: {
+        loadAssets(assets?: any[], cb?: any): void;
+        setAssetsPath(path?: string): void;
+        setPath(path?: string): void;
+        loaded(): void;
+        destroy(): void;
+        getImage(assetData: any): void;
+        _createImageData(assetData: any): void;
+        _imageLoaded(): void;
+        assetsPath: string;
+        path: string;
+        totalImages: number;
+        loadedAssets: number;
+        imagesLoadedCb: any;
+        images: any[];
+    }
+    play(name?: string): void;
+    stop(name?: string): void;
+    togglePause(name?: string): void;
+    destroy(name?: string): void;
+    pause(name?: string): void;
     resize(): void;
     setSpeed(speed: number): void;
-    goToAndPlay(value: number, isFrame?: boolean): void;
-    goToAndStop(value: number, isFrame?: boolean): void;
+    goToAndStop(value: number, isFrame?: boolean, name?: string): void;
+    goToAndPlay(value: number, isFrame?: boolean, name?: string): void;
     setDirection(direction: AnimationDirection): void;
     playSegments(segments: AnimationSegment | AnimationSegment[], forceFlag?: boolean): void;
     setSubframe(useSubFrames: boolean): void;
-    destroy(): void;
     getDuration(inFrames?: boolean): number;
     triggerEvent<T = any>(name: AnimationEventName, args: T): void;
     addEventListener<T = any>(name: AnimationEventName, callback: AnimationEventCallback<T>): void;
     removeEventListener<T = any>(name: AnimationEventName, callback: AnimationEventCallback<T>): void;
+    setParams(params: AnimationConfigWithPath | AnimationConfigWithData): void;
+    setData(wrapper: Element, animationData: any): void;
+    includeLayers(data: any): void;
+    loadNextSegment(): void;
+    loadSegments(): void;
+    imagesLoaded(): void;
+    preloadImages(): void;
+    configAnimation(animData: any): void,
+    waitForFontsLoaded(): void;
+    checkLoaded(): void;
+    gotoFrame(): void;
+    renderFrame(): void;
+    advanceTime(value: number): void;
+    adjustSegment(arr: AnimationSegment | AnimationSegment[], offset: number): void;
+    setSegment(init: number, end: number): void;
+    resetSegments(forceFlag: boolean): void;
+    checkSegments(offset: number): boolean;
+    setCurrentRawFrameValue(value: number): void;
+    updaFrameModifier(): void;
+    getPath(): string;
+    getAssetsPath(): string;
+    getAssetData(id: any): string;
+    hide(): void;
+    show(): void;
+    trigger(name: AnimationEventName): void;
+    triggerRenderFrameError:(nativeError: any) => void;
+    triggerConfigError:(nativeError: any) => void;
 }
 
 export type BaseRendererConfig = {
