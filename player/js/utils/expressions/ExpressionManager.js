9d24dcdfa179d07afe22a522e5ad19bbef23423b
airnan <hernantorrisi@gmail.com>
2021-05-31 18:16:35 -0400

    expressions update

--- player/js/utils/expressions/ExpressionManager.js
+++ player/js/utils/expressions/ExpressionManager.js
@@ -8,6 +8,8 @@ var ExpressionManager = (function () {
   var Math = BMMath;
   var window = null;
   var document = null;
+  var XMLHttpRequest = null;
+  var fetch = null;
 
   function $bm_isInstanceOfArray(arr) {
     return arr.constructor === Array || arr.constructor === Float32Array;
@@ -410,7 +412,7 @@ var ExpressionManager = (function () {
     var velocityAtTime;
 
     var scoped_bm_rt;
-    val = val.replace(/(\\?"|')((http)(s)?(:\/))?\/.*?(\\?"|')/g, "\"\""); // deter potential network calls
+    // val = val.replace(/(\\?"|')((http)(s)?(:\/))?\/.*?(\\?"|')/g, "\"\""); // deter potential network calls
     var expression_function = eval('[function _expression_function(){' + val + ';scoped_bm_rt=$bm_rt}]')[0]; // eslint-disable-line no-eval
     var numKeys = property.kf ? data.k.length : 0;
 
