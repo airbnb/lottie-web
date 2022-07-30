119a08405707b4cafcb34d3601895e5f20d844b8
Mattia Basaglia <glax@dragon.best>
2022-07-30 11:59:21 +0200

    Smooth zig zag

--- player/js/utils/shapes/ZigZagModifier.js
+++ player/js/utils/shapes/ZigZagModifier.js
@@ -14,7 +14,10 @@ ZigZagModifier.prototype.initModifierProperties = function (elem, data) {
   this.getValue = this.processKeys;
   this.amplitude = PropertyFactory.getProp(elem, data.s, 0, null, this);
   this.frequency = PropertyFactory.getProp(elem, data.r, 0, null, this);
-  this._isAnimated = this.amplitude.effectsSequence.length !== 0 && this.frequency.effectsSequence.length !== 0;
+  this.pointType = PropertyFactory.getProp(elem, data.pt, 0, null, this);
+  this._isAnimated = this.pointType.effectsSequence.length !== 0
+    && this.amplitude.effectsSequence.length !== 0
+    && this.frequency.effectsSequence.length !== 0;
 };
 
 function angleMean(a, b) {
@@ -23,7 +26,23 @@ function angleMean(a, b) {
   return (a + b) / 2;
 }
 
-function zigZagCorner(outputBezier, segmentBefore, segmentAfter, amplitude, direction) {
+function zigZagAddPoint(outputBezier, px, py, tanLen, tanAngle) {
+  var tix = px;
+  var tiy = py;
+  var tox = px;
+  var toy = py;
+
+  if (tanLen !== 0) {
+    tix += Math.cos(tanAngle) * tanLen;
+    tiy -= Math.sin(tanAngle) * tanLen;
+    tox -= Math.cos(tanAngle) * tanLen;
+    toy += Math.sin(tanAngle) * tanLen;
+  }
+
+  outputBezier.setTripleAt(px, py, tox, toy, tix, tiy, outputBezier.length());
+}
+
+function zigZagCorner(outputBezier, segmentBefore, segmentAfter, amplitude, direction, tanLen) {
   var point;
   var angle;
 
@@ -40,10 +59,10 @@ function zigZagCorner(outputBezier, segmentBefore, segmentAfter, amplitude, dire
 
   var px = point[0] + Math.cos(angle) * direction * amplitude;
   var py = point[1] - Math.sin(angle) * direction * amplitude;
-  outputBezier.setTripleAt(px, py, px, py, px, py, outputBezier.length());
+  zigZagAddPoint(outputBezier, px, py, tanLen, angle + Math.PI / 2);
 }
 
-function zigZagSegment(outputBezier, segment, amplitude, frequency, direction) {
+function zigZagSegment(outputBezier, segment, amplitude, frequency, direction, tanLen) {
   for (var i = 0; i < frequency; i += 1) {
     var f = (i + 1) / (frequency + 1);
     var t = segment.tAtLengthPercent(f);
@@ -51,8 +70,7 @@ function zigZagSegment(outputBezier, segment, amplitude, frequency, direction) {
     var point = segment.point(t);
     var px = point[0] + Math.cos(angle) * direction * amplitude;
     var py = point[1] - Math.sin(angle) * direction * amplitude;
-
-    outputBezier.setTripleAt(px, py, px, py, px, py, outputBezier.length());
+    zigZagAddPoint(outputBezier, px, py, tanLen, angle + Math.PI / 2);
 
     direction = -direction;
   }
@@ -60,7 +78,7 @@ function zigZagSegment(outputBezier, segment, amplitude, frequency, direction) {
   return direction;
 }
 
-ZigZagModifier.prototype.processPath = function (path, amplitude, frequency) {
+ZigZagModifier.prototype.processPath = function (path, amplitude, frequency, smooth) {
   var count = path._length;
   var clonedPath = shapePool.newElement();
   clonedPath.c = path.c;
@@ -75,12 +93,15 @@ ZigZagModifier.prototype.processPath = function (path, amplitude, frequency) {
   var segment = path.c ? PolynomialBezier.shapeSegment(path, count - 1) : null;
   var nextSegment = PolynomialBezier.shapeSegment(path, 0);
 
-  zigZagCorner(clonedPath, segment, nextSegment, amplitude, -1);
+  segment.calculateLengthData();
+  var tanLen = smooth ? segment.length / (frequency + 1) / 2 : 0;
+
+  zigZagCorner(clonedPath, segment, nextSegment, amplitude, -1, tanLen);
 
   for (var i = 0; i < count; i += 1) {
     segment = nextSegment;
 
-    direction = zigZagSegment(clonedPath, segment, amplitude, frequency, -direction);
+    direction = zigZagSegment(clonedPath, segment, amplitude, frequency, -direction, tanLen);
 
     if (i === count - 1 && !path.c) {
       nextSegment = null;
@@ -88,7 +109,7 @@ ZigZagModifier.prototype.processPath = function (path, amplitude, frequency) {
       nextSegment = PolynomialBezier.shapeSegment(path, (i + 1) % count);
     }
 
-    zigZagCorner(clonedPath, segment, nextSegment, amplitude, direction);
+    zigZagCorner(clonedPath, segment, nextSegment, amplitude, direction, tanLen);
   }
 
   return clonedPath;
@@ -102,6 +123,7 @@ ZigZagModifier.prototype.processShapes = function (_isFirstFrame) {
   var jLen;
   var amplitude = this.amplitude.v;
   var frequency = Math.max(0, Math.round(this.frequency.v));
+  var smooth = this.pointType.v === 2;
 
   if (amplitude !== 0) {
     var shapeData;
@@ -115,7 +137,7 @@ ZigZagModifier.prototype.processShapes = function (_isFirstFrame) {
         shapePaths = shapeData.shape.paths.shapes;
         jLen = shapeData.shape.paths._length;
         for (j = 0; j < jLen; j += 1) {
-          localShapeCollection.addShape(this.processPath(shapePaths[j], amplitude, frequency));
+          localShapeCollection.addShape(this.processPath(shapePaths[j], amplitude, frequency, smooth));
         }
       }
       shapeData.shape.paths = shapeData.localShapeCollection;
