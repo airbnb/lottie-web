b63db4bd2be739cd40fcc0f24c35045c4ab4c12d
airnan <hernantorrisi@gmail.com>
2022-08-20 14:28:14 -0400

    WIP slots

---
+++ player/js/utils/SlotManager.js
@@ -0,0 +1,17 @@
+function SlotManager(animationData) {
+  this.animationData = animationData;
+}
+SlotManager.prototype.getProp = function (data) {
+  if (this.animationData.slots
+    && this.animationData.slots[data.pid]
+  ) {
+    return Object.assign(data, this.animationData.slots[data.pid]);
+  }
+  return data;
+};
+
+function slotFactory(animationData) {
+  return new SlotManager(animationData);
+}
+
+export default slotFactory;
