f032baae7b208b928acdeaa9e0cc0508bb7b6517
Richard Henry <richardhenry@me.com>
2022-12-21 18:35:21 -0600

    [canvas renderer] fix blurry CGImageElement drawing on high dpi displays

--- player/js/elements/canvasElements/CVImageElement.js
+++ player/js/elements/canvasElements/CVImageElement.js
@@ -1,7 +1,6 @@
 import {
   extendPrototype,
 } from '../../utils/functionExtensions';
-import createTag from '../../utils/helpers/html_elements';
 import RenderableElement from '../helpers/RenderableElement';
 import BaseElement from '../BaseElement';
 import TransformElement from '../helpers/TransformElement';
@@ -21,34 +20,25 @@ extendPrototype([BaseElement, TransformElement, CVBaseElement, HierarchyElement,
 CVImageElement.prototype.initElement = SVGShapeElement.prototype.initElement;
 CVImageElement.prototype.prepareFrame = IImageElement.prototype.prepareFrame;
 
-CVImageElement.prototype.createContent = function () {
-  if (this.img.width && (this.assetData.w !== this.img.width || this.assetData.h !== this.img.height)) {
-    var canvas = createTag('canvas');
-    canvas.width = this.assetData.w;
-    canvas.height = this.assetData.h;
-    var ctx = canvas.getContext('2d');
-
-    var imgW = this.img.width;
-    var imgH = this.img.height;
-    var imgRel = imgW / imgH;
-    var canvasRel = this.assetData.w / this.assetData.h;
-    var widthCrop;
-    var heightCrop;
-    var par = this.assetData.pr || this.globalData.renderConfig.imagePreserveAspectRatio;
-    if ((imgRel > canvasRel && par === 'xMidYMid slice') || (imgRel < canvasRel && par !== 'xMidYMid slice')) {
-      heightCrop = imgH;
-      widthCrop = heightCrop * canvasRel;
-    } else {
-      widthCrop = imgW;
-      heightCrop = widthCrop / canvasRel;
-    }
-    ctx.drawImage(this.img, (imgW - widthCrop) / 2, (imgH - heightCrop) / 2, widthCrop, heightCrop, 0, 0, this.assetData.w, this.assetData.h);
-    this.img = canvas;
-  }
-};
-
 CVImageElement.prototype.renderInnerContent = function () {
-  this.canvasContext.drawImage(this.img, 0, 0);
+  var imgW = this.img.width;
+  var imgH = this.img.height;
+
+  var imgRel = imgW / imgH;
+  var canvasRel = this.assetData.w / this.assetData.h;
+  var widthCrop;
+  var heightCrop;
+  var par = this.assetData.pr || this.globalData.renderConfig.imagePreserveAspectRatio;
+
+  if ((imgRel > canvasRel && par === 'xMidYMid slice') || (imgRel < canvasRel && par !== 'xMidYMid slice')) {
+    heightCrop = imgH;
+    widthCrop = heightCrop * canvasRel;
+  } else {
+    widthCrop = imgW;
+    heightCrop = widthCrop / canvasRel;
+  }
+
+  this.canvasContext.drawImage(this.img, (imgW - widthCrop) / 2, (imgH - heightCrop) / 2, widthCrop, heightCrop, 0, 0, this.assetData.w, this.assetData.h);
 };
 
 CVImageElement.prototype.destroy = function () {
