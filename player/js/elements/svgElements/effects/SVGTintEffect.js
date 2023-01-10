45f1edad2801eff417e9900a57240c40b69a4284
airnan <hernantorrisi@gmail.com>
2023-01-10 06:47:13 -0500

    fix for tint effect opacity

--- player/js/elements/svgElements/effects/SVGTintEffect.js
+++ player/js/elements/svgElements/effects/SVGTintEffect.js
@@ -4,12 +4,15 @@ import {
   extendPrototype,
 } from '../../../utils/functionExtensions';
 
+var linearFilterValue = '0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0 0 0';
+
 function SVGTintFilter(filter, filterManager, elem, id, source) {
   this.filterManager = filterManager;
   var feColorMatrix = createNS('feColorMatrix');
   feColorMatrix.setAttribute('type', 'matrix');
   feColorMatrix.setAttribute('color-interpolation-filters', 'linearRGB');
-  feColorMatrix.setAttribute('values', '0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0 0 0 1 0');
+  feColorMatrix.setAttribute('values', linearFilterValue + ' 1 0');
+  this.linearFilter = feColorMatrix;
   feColorMatrix.setAttribute('result', id + '_tint_1');
   filter.appendChild(feColorMatrix);
   feColorMatrix = createNS('feColorMatrix');
@@ -36,7 +39,8 @@ SVGTintFilter.prototype.renderFrame = function (forceRender) {
     var colorBlack = this.filterManager.effectElements[0].p.v;
     var colorWhite = this.filterManager.effectElements[1].p.v;
     var opacity = this.filterManager.effectElements[2].p.v / 100;
-    this.matrixFilter.setAttribute('values', (colorWhite[0] - colorBlack[0]) + ' 0 0 0 ' + colorBlack[0] + ' ' + (colorWhite[1] - colorBlack[1]) + ' 0 0 0 ' + colorBlack[1] + ' ' + (colorWhite[2] - colorBlack[2]) + ' 0 0 0 ' + colorBlack[2] + ' 0 0 0 ' + opacity + ' 0');
+    this.linearFilter.setAttribute('values', linearFilterValue + ' ' + opacity + ' 0');
+    this.matrixFilter.setAttribute('values', (colorWhite[0] - colorBlack[0]) + ' 0 0 0 ' + colorBlack[0] + ' ' + (colorWhite[1] - colorBlack[1]) + ' 0 0 0 ' + colorBlack[1] + ' ' + (colorWhite[2] - colorBlack[2]) + ' 0 0 0 ' + colorBlack[2] + ' 0 0 0 1 0');
   }
 };
 
