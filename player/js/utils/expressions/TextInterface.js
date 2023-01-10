653867ce4e076b23a580edf74bcef91ccec6bca1
airnan <hernantorrisi@gmail.com>
2023-01-10 06:47:43 -0500

    add support for fill color property

--- player/js/utils/expressions/TextInterface.js
+++ player/js/utils/expressions/TextInterface.js
@@ -15,10 +15,17 @@ const TextExpressionInterface = (function () {
         elem.textProperty.getValue();
         var stringValue = elem.textProperty.currentData.t;
         if (stringValue !== _prevValue) {
-          elem.textProperty.currentData.t = _prevValue;
+          _prevValue = elem.textProperty.currentData.t;
           _sourceText = new String(stringValue); // eslint-disable-line no-new-wrappers
           // If stringValue is an empty string, eval returns undefined, so it has to be returned as a String primitive
           _sourceText.value = stringValue || new String(stringValue); // eslint-disable-line no-new-wrappers
+          Object.defineProperty(_sourceText, 'style', {
+            get: function () {
+              return {
+                fillColor: elem.textProperty.currentData.fc,
+              };
+            },
+          });
         }
         return _sourceText;
       },
